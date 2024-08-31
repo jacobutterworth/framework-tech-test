61f77cb4ed869617a1d90a0e5f040c55cf578b41
JB-MyLocal <jacob@thelincolnite.co.uk>
2024-08-31 12:02:27 +0100

    fix: yoink cn util from shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Open_Sans({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, "bg-white")}>{children}</body>
     </html>
   );
 }
