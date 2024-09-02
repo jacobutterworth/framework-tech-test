c33f9139e7d475d2848cf22cb4e663f0064655ff
JB-MyLocal <jacob@thelincolnite.co.uk>
2024-09-02 15:42:14 +0100

    feat: add components to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import Button from "@/components/Button";
+import NavBarButton from "@/components/NavBarButton";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <Button title="About us" />
+        <NavBarButton title="Nav button 01" />
+      </div>
+    </main>
   );
 }
