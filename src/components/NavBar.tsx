aa9dac7b5cffce278fa2b58f9a240fd3f0838be8
JB-MyLocal <jacob@thelincolnite.co.uk>
2024-09-02 21:20:11 +0100

    please see previous

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { cn } from "@/lib/utils";
-// components/Navbar.tsx
 import { useState } from "react";
 import NavBarButton from "./NavBarButton";
 
@@ -8,29 +7,54 @@ const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="w-full px-4 py-4 text-white flex justify-between items-center relative">
-      <span className="title-text text-green-100">COTTAGE RETREAT</span>
-      <div
-        className={cn(
-          `md:flex md:items-center md:gap-4 md:h-auto`,
-          isOpen
-            ? "top-0 left-0 w-screen h-screen bg-green-100 flex flex-col"
-            : "hidden"
-        )}
-      >
-        <NavBarButton title="Nav Button 01" />
-        <NavBarButton title="Nav Button 02" />
-        <NavBarButton title="Nav Button 03" />
-      </div>
-      <div className="md:hidden">
+    <nav className="w-full px-4 py-4 text-white">
+      <div className="flex justify-between items-center">
+        <span className="title-text text-green-100">COTTAGE RETREAT</span>
+
+        <div className="hidden md:flex md:items-center md:gap-4">
+          <NavBarButton title="Nav Button 01" />
+          <NavBarButton title="Nav Button 02" />
+          <NavBarButton title="Nav Button 03" />
+        </div>
+
         <button
+          className="md:hidden text-2xl text-green-100"
           onClick={() => setIsOpen(!isOpen)}
-          className="text-2xl text-green-100"
         >
           &#9776;
         </button>
       </div>
-    </div>
+
+      <div
+        className={cn(
+          "fixed inset-0 z-40 bg-green-100 flex flex-col",
+          "transition-opacity duration-300 ease-in-out",
+          isOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        )}
+      >
+        <div className="flex justify-end z-50 md:px-28 px-4 py-4">
+          <button
+            className="absolute md:px-28 px-5 text-4xl"
+            onClick={() => setIsOpen(false)}
+          >
+            &times;
+          </button>
+        </div>
+        <div
+          className={cn(
+            "flex flex-col items-center gap-4",
+            "transition-transform duration-300 ease-in-out",
+            isOpen ? "translate-y-0" : "translate-y-full"
+          )}
+        >
+          <NavBarButton title="Nav Button 01" />
+          <NavBarButton title="Nav Button 02" />
+          <NavBarButton title="Nav Button 03" />
+        </div>
+      </div>
+    </nav>
   );
 };
 
