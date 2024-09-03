c45fac5d73519e554b26a0391b3c05e23f084941
JB-MyLocal <jacob@thelincolnite.co.uk>
2024-09-03 15:25:42 +0100

    fix legal links on mobile

--- src/components/footer/LegalLinks.tsx
+++ src/components/footer/LegalLinks.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const LegalLinks: React.FC = () => {
   return (
-    <div className="text-right text-green-100 flex flex-col gap-2 ml-auto">
-      <span className="copy-s self-end">Privacy</span>
-      <span className="copy-s self-end">Terms and Conditions</span>
+    <div className="text-right md:text-green-100 text-white-100 flex flex-col gap-2 md:ml-auto">
+      <span className="copy-s md:self-end">Privacy</span>
+      <span className="copy-s md:self-end">Terms and Conditions</span>
     </div>
   );
 };
