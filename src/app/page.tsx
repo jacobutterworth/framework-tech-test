6bc90ecfc9fc37800367ebf07bdb96c450e5e069
JB-MyLocal <jacob@thelincolnite.co.uk>
2024-09-03 13:38:44 +0100

    feat: add accordion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Accordion from "@/components/Accordion";
 import Button from "@/components/Button";
 import NavBarButton from "@/components/NavBarButton";
 
@@ -5,11 +6,29 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="flex flex-col items-center justify-center gap-4">
-        <Button title="About us" />
-        <NavBarButton
-          title="Nav button 01"
-          manualTextColor={"text-green-100"}
-        />
+        <div className="my-12">
+          <Accordion
+            items={[
+              {
+                id: 1,
+                title:
+                  "Lorem ipsum dolor sit amet consectetur. Ac posuere accumsan?",
+                content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              },
+              {
+                id: 2,
+                title:
+                  "Lorem ipsum dolor sit amet consectetur. Ac posuere accumsan?",
+                content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              },
+            ]}
+          />
+        </div>
+        <div className="self-start">
+          <Button title="CTA Button" />
+        </div>
       </div>
     </main>
   );
---
+++ src/components/Accordion.tsx
@@ -0,0 +1,65 @@
+"use client";
+import React, { useState } from "react";
+
+interface AccordionItem {
+  id: number;
+  title: string;
+  content: string;
+}
+
+interface AccordionProps {
+  items: AccordionItem[];
+}
+
+const Accordion: React.FC<AccordionProps> = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const toggleAccordion = (index: number) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
+  return (
+    <div className="space-y-4 w-full ">
+      {items.map((item, index) => (
+        <div key={item.id} className="border-b border-gray-200 pb-4">
+          <div
+            className="flex justify-between items-center py-2 cursor-pointer"
+            onClick={() => toggleAccordion(index)}
+          >
+            <div className="flex items-center gap-2 flex-grow overflow-hidden">
+              <span className="text-gray-500 text-lg flex-shrink-0">{`0${item.id}`}</span>
+              <h3 className="text-lg font-medium text-green-100 flex-wrap mx-4">
+                {item.title}
+              </h3>
+            </div>
+            <div className="flex-shrink-0 relative w-6 h-6">
+              <span
+                className={`absolute inset-0 text-2xl text-green-100 transition-opacity duration-300 ease-in-out ${
+                  activeIndex === index ? "opacity-0" : "opacity-100"
+                }`}
+              >
+                +
+              </span>
+              <span
+                className={`absolute inset-0 text-2xl text-gray-500 transition-opacity duration-300 ease-in-out ${
+                  activeIndex === index ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                −
+              </span>
+            </div>
+          </div>
+          <div
+            className={` pl-10 text-gray-600 overflow-hidden transition-all duration-300 ease-in-out ${
+              activeIndex === index ? "max-h-96" : "max-h-0"
+            }`}
+          >
+            <div className="overflow-y-auto copy max-h-96">{item.content}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
